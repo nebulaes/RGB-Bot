b6330d01069197b051388491b43df5b43cfc1b42
antonynaumovic <40827853+antonynaumovic@users.noreply.github.com>
2018-08-28 01:34:35 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -77,6 +77,7 @@ async def on_ready():
     
     embed=discord.Embed(title="Ranks")
     embed.add_field(name=" ", value="React With Appropriate Reaction To Add Role", inline=False)
+    print(r)
     msgRank = await bot.send_message(r, embed=embed)
     await bot.add_reaction(msgRank, emojiCop)
     await bot.add_reaction(msgRank, emojiBro)
