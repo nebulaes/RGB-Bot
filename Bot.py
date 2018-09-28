f18dca96857ac23d3281972a0c828e6ff0ee427c
antonynaumovic <40827853+antonynaumovic@users.noreply.github.com>
2018-09-29 00:04:18 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -121,8 +121,16 @@ async def on_ready():
 async def on_member_join(member):
     for server in bot.servers:
         for channel in server.channels:
+            h = 0
             if channel.id == "463785244772794370" and member.server.name == "Nebula eSports":
                 w = channel
+            if channel.id == "495362269736075273" and member.server.name == "Nebula eSports":
+                zc = channel
+            elif channel.id == "495363675830616073" and member.server.name == "Nebula eSports":
+                bc = channel
+            elif channel.id == "495367099024277515" and member.server.name == "Nebula eSports":
+                tc = channel
+
                 print("{} Has Joined".format(member))
                 embed=discord.Embed(title=str(member.name), color=0xfa9361)
                 embed.set_author(name="Welcome To Nebula:")
@@ -132,6 +140,29 @@ async def on_member_join(member):
                 role2 = discord.utils.get(member.server.roles, name="⋑-Members-⋐")
                 await bot.add_roles(member, role)
                 await bot.add_roles(member, role2)
+
+                
+                t = 0
+                b = 0
+                for i in member.server.members:
+                    t += 1
+                    if i.bot == True:
+                        b +=1
+                h = t - b
+                await bot.edit_channel(channel = tc, name="╠-total-﹝{}﹞".format(t))
+                await bot.edit_channel(channel = zc, name="╠-members-﹝{}﹞".format(h))
+                await bot.edit_channel(channel = bc, name="╠-bots-﹝{}﹞".format(b))
+
+@bot.command(pass_context=True)
+async def memberCount(ctx):
+    for server in bot.servers:
+        for channel in server.channels:
+            if channel.id == "495362269736075273":
+                zc = channel
+            elif channel.id == "495363675830616073":
+                bc = channel
+            elif channel.id == "495367099024277515":
+                tc = channel
     
     
 @bot.command(pass_context=True)
